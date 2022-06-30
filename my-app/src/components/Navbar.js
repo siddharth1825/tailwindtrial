ad4dd324bb86cf81907fbbbb6784f542857f5520
siddharth1825 <siddharth.dave2012@gmail.com>
2022-06-30 12:41:15 -0400

    navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function Navbar({toggle}) {
     return (
-        <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm text-xl">
+        <nav className="flex justify-between items-center h-16 bg-black text-white relative shadow-sm text-xl">
             <Link to='/' className="pl-8 ">Bruh</Link>
             <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
