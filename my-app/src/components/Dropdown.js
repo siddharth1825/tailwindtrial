507c1993e0f1b2cc8c68a20b4b0dc0532feedba6
siddharth1825 <siddharth.dave2012@gmail.com>
2022-06-30 13:03:33 -0400

    dropdown borders

--- my-app/src/components/Dropdown.js
+++ my-app/src/components/Dropdown.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 const Dropdown = ({open , toggle}) => {
   return (
     <div className={open ? "grid grid-rows-4 text-center items-center bg-red-500" : "hidden"} onClick={toggle}>
-        <Link className="p-4" to="/">Home</Link>
-        <Link className="p-4" to="/Menu">Menu</Link>
-         <Link className="p-4" to="/About">About</Link>
-        <Link className="p-4" to="/Contact">Contact</Link>
+        <Link className="p-4 shadow-md" to="/">Home</Link>
+        <Link className="p-4 shadow-md" to="/Menu">Menu</Link>
+         <Link className="p-4 shadow-md" to="/About">About</Link>
+        <Link className="p-4 shadow-md" to="/Contact">Contact</Link>
     </div>
   )
 }
